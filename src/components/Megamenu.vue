859c04aa6048b9774007effbc1af90e7f774370a
marzieh alishahi <marziehalishahi1999@gmail.com>
2023-04-04 22:37:56 +0430

    working on megamenu

--- src/components/Megamenu.vue
+++ src/components/Megamenu.vue
@@ -2,66 +2,72 @@
 </script>
 <template>
     <div class="menubar__megamenu megamenu">
-        <div class="flex justify-center gap-5 ">
-
-            <div class="megamenu__column1">
-                <h4>home page1</h4>
-                <div>home1</div>
-                <div>
-                    <span class="ml-16">home2</span>
-                    <span class="ml-1"></span>
-                    <span><a class="megamenu__column1__home__new">new!</a></span>
-                </div>
-                <div>home3</div>
-                <div>
-                    <span class="ml-16">home2</span>
-                    <span class="ml-1"></span>
-                    <span><a class="megamenu__column1__home__new">new!</a></span>
-                </div>
-                <div>home5</div>
-            </div>
-
-            <div class="megamenu__column1">
-                <h4>home page2</h4>
-                <div>home1</div>
-                <div>home2</div>
-                <div>home3</div>
-                <div>home4</div>
-                <div>home5</div>
-            </div>
-
-            <div class="megamenu__column1">
-                <h4>home page3</h4>
-                <div>home1</div>
-                <div>home2</div>
-                <div>home3</div>
-                <div>home4</div>
-                <div>home5</div>
-            </div>
-
-            <div class="megamenu__column1">
-                <h4>home page4</h4>
-                <div>home1</div>
-                <div>home2</div>
-                <div>home3</div>
-                <div>home4</div>
-                <div>home5</div>
-            </div>
-            <div class="flex justify-between mb-3 megamenu__column2">
-                <div>
-                    <h5>sale collection</h5>
+        <div class="megamenu_container">
+            <div class="flex justify-between gap-5">
+                <div class="megamenu__column">
+                    <h4>home page1</h4>
+                    <div>home1</div>
                     <div>
-                        <p>up to 80% off retail</p>
+                        <span>home2</span>
+                        <span class="ml-1"></span>
+                        <span><a class="megamenu__column_home_new">new!</a></span>
                     </div>
-                    <div class="flex justify-between mb-4 megamenue__column2__image">
-                        <div><img src="../assets/images/12.jpg"></div>
+                    <div>home3</div>
+                    <div>
+                        <span>home2</span>
+                        <span class="ml-1"></span>
+                        <span><a class="megamenu__column_home_new">new!</a></span>
                     </div>
-                    <div class="megamenue__ column2__show">
-                        <a class="column2__show">show me all</a>
+                    <div>home5</div>
+                </div>
+
+                <div class="megamenu__column">
+                    <h4>home page2</h4>
+                    <div>home1</div>
+                    <div>home2</div>
+                    <div>home3</div>
+                    <div>home4</div>
+                    <div>home5</div>
+                </div>
+
+                <div class="megamenu__column">
+                    <h4>home page3</h4>
+                    <div>home1</div>
+                    <div>home2</div>
+                    <div>home3</div>
+                    <div>home4</div>
+                    <div>home5</div>
+                </div>
+
+                <div class="megamenu__column">
+                    <h4>home page4</h4>
+                    <div>home1</div>
+                    <div>home2</div>
+                    <div>home3</div>
+                    <div>home4</div>
+                    <div>home5</div>
+                </div>
+                <div>
+                    <div class="megamenu_image_holder image_holder">
+                        <div class="image_holder__desc">
+                            <h5>sale collection</h5>
+                            <p class="text-4xl font-bold break-normal">up to 80% off retail</p>
+                            <div class="megamenue__ column2__show">
+                                <a class="column2__show">show me all</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="flex justify-between mb-4 image__holder_container">
+                                <div><img class="object-contain w-[225px] h-auto" src="../../images/12.jpg"></div>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
+
 
     </div>
 </template>
@@ -83,34 +89,53 @@
     box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 }
 
-.megamenu__column1 {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-left: 2rem;
-    margin-right:8rem;
-    cursor: pointer
+.megamenu_container {
+    width: 1200px;
+    margin: 0 auto;
 }
 
- .megamenu__column1__home__new{
+.megamenu__column {
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    /* margin-left: 2rem;
+    margin-right: 8rem; */
+    cursor: pointer;
+    gap: 1rem;
+}
+
+.megamenu__column_home_new {
     border: 1px solid none;
     border-radius: 8px;
     padding: 0px;
-    margin-right:20px;
+    margin-right: 20px;
     /* display: inline-block; */
     width: 40%;
     text-align: center;
     background-color: red;
-    color:white;
-    outline:red;
-
+    color: white;
+    outline: red;
 }
-.megamenu__column2 {
+
+.image_holder {
     display: flex;
+    justify-content: space-between;
     cursor: pointer;
-    flex-direction: column;
     background-color: rgb(239, 244, 248);
     padding: 26px;
+}
+
+/* .image_holder__desc:nth-child(n){
+    margin-top: 4px;
+}
+.image_holder__desc:last-child{
+    margin-top: 9px;
+} */
+.image_holder__desc {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    /* gap: 10px; */
 }
 
 .column2__show {
@@ -137,5 +162,4 @@
     border: none;
     outline: none;
     transform: scale(1.1, 1.1);
-}
-</style>
+}</style>
