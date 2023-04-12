598f3d679021b3ddfd3af928d3a17e51348f4069
marzieh alishahi <marziehalishahi1999@gmail.com>
2023-04-12 18:43:49 +0430

    working on navbar and userprofile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,10 +8,17 @@ import UserProfile from '@/UserProfile.vue'
 
 <template>
     <nav class="menubar">
+        <div class="hamburger-menu__container">
+            <div class="burger1"></div>
+            <div class="burger2"></div>
+            <div class="burger3"></div>
+        </div>
+
+        
         <div class="menubar__logo">
             <img src="../assets/images/images.svg">
         </div>
-        <div class="flex items-center h-full">
+        <div class="h-full menubar__container">
             <ul class="h-full menubar__items">
                 <li class="menubar__list"><span class="menubar__background">Men</span> </li>
                 <li class="menubar__list"><span class="menubar__background">Women </span> </li>
@@ -36,7 +43,50 @@ import UserProfile from '@/UserProfile.vue'
                             <li>Men </li>
                             <li>Women
                                 <ChevronDownIcon class="inline-block w-4 h-4 text-gray-400" />
-                                <ul class="menubar__items--subtype"></ul>
+                                <div>
+                                    <ul class="menubar__items--subtype">
+                                    <li>Home Demo 1</li>
+                                    <li>Home Demo 2</li>
+                                    <li>Category Pages
+                                        <ChevronDownIcon class="inline-block w-4 h-4 text-gray-400" />
+                                        <ul class="menubar__items--subtype"> 
+                                            <li>Category Pages 1</li>
+                                            <li>Category Pages 2</li>
+
+                                        </ul>
+                                    </li>
+                                    <li>Product Pages
+                                        <ChevronDownIcon class="inline-block w-4 h-4 text-gray-400" />
+                                        <ul class="menubar__items--subtype">
+                                            <li>Product Pages 1</li>
+                                            <li>Product Pages 2</li>
+
+                                        </ul>
+                                    </li>
+                                    <li>Cart Page</li>
+                                    <li>Checkout Page</li>
+                                    <li>Search Page</li>
+                                    <li>Account page</li>
+                                    <li>Other Pages
+                                        <ChevronDownIcon class="inline-block w-4 h-4 text-gray-400" />
+                                        <ul class="menubar__items--subtype">
+                                            <li>About</li>
+                                            <li>Contact us</li>
+                                            <li>Login</li>
+                                            <li>Signup</li>
+                                            <li>Subscription</li>
+
+                                        </ul>
+                                    </li>
+                                    <li>Blog Page
+                                        <ChevronDownIcon class="inline-block w-4 h-4 text-gray-400" />
+                                        <ul class="menubar__items--subtype">
+                                            <li>Blog Page</li>
+                                            <li>Blog Single</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                                </div>
                             </li>
                             <li>Beauty</li>
                             <li>Sport</li>
@@ -135,6 +185,7 @@ ul.menubar__explore--items{
     transition: all 100ms ease-in-out;
     transition-delay: 100ms;
     top: 40px;
+    border-radius: 8px;
 }
 .menubar__list.menubar__list--megamenu:hover .menubar__megamenu{
     transform: scale(1, 1);
@@ -151,7 +202,7 @@ ul.menubar__explore--items{
 }
 
 .menubar__explore--items li:hover {
-    background-color: aqua;
+    background-color: rgb(238, 247, 247);
     cursor: pointer;
 }
 ul.menubar__items--subtype{
@@ -161,12 +212,59 @@ ul.menubar__items--subtype{
     transform: scale(0, 1);
     transition: transform 100ms ease-in-out;
     transform-origin: left center;
-    background-color: black;
-    height: 100px;
+    background-color:white;
+    height: auto;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    border-radius: 8px;
 }
 .menubar__explore--items li:hover ul.menubar__items--subtype{
     transform: scale(1, 1)
 }
 
+.menubar__container{
+    display: flex;
+    justify-content: center;
+}
 
+.hamburger-menu__container{
+    /* opacity: 0; */
+    position: relative;
+}
+/* .hamburger-menu__container span{
+    margin-bottom: 50px;
+} */
+.hamburger-menu__container div::after {
+    content: '';
+    position: absolute;
+    width: 20px;
+    height: 1px;
+    background-color: black;
+    z-index: 1000;
+}
+.hamburger-menu__container .burger1::after{
+    top: 10px;
+    left: -100px;
+}
+
+.hamburger-menu__container .burger2::after{
+    top: 14px;
+    left: -100px;
+}
+
+.hamburger-menu__container .burger3::after{
+    top: 19px;
+    left: -100px;
+}
+
+@media screen and (max-width: 1024px) {
+
+    .menubar__container, .menubar__logo{
+        display: none;
+    }
+
+    nav.menubar{
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+}
 </style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,26 +1,68 @@
 <script setup>
-    import { ref } from 'vue';
-    import { UserIcon } from '@heroicons/vue/24/outline';
-    let profile = ref(false)
-    function toggle(){
-        profile.value = !profile.value
-    }
+import { ref } from 'vue';
+import { UserIcon } from '@heroicons/vue/24/outline';
+let profile = ref(false)
+function toggle() {
+    profile.value = !profile.value
+}
 
 </script>
 
 
 <template>
-    
-        <UserIcon @click="toggle" class="w-6 h-6 text-black"></UserIcon>
-        <div class="relative">
-            <div :class="{'hidden' : !profile}" class="absolute text-white bg-black rounded-sm shadow-md w-36 h-44 top-8">
-            safdsf
+    <UserIcon @click="toggle" class="w-6 h-6 text-black"></UserIcon>
+    <div class="relative">
+        <div :class="{ 'hidden': !profile }" class="absolute h-auto text-black bg-white rounded-lg shadow-md text w-36 top-8">
+            <div class="flex justify-center">
+                <div class="UserProfile__items">
+                    <div class="UserProfile__item1">
+                        <span class="UserProfile__item1__img"><img class="rounded-sm h-11" src="../../images/12.jpg"></span>
+                        <h5 class="text-base font-bold">Eden Smith</h5>
+                        <p class="text-base font-medium">LosAngeles,CA</p>
+                        <div class="w-full border-b border-neutral-200 dark:border-neutral-700"></div>
+                    </div>
+                    <div class="UserProfile__item2">
+                        <h5 class="text-base font-bold">My Account</h5>
+                        <h5 class="text-base font-bold">My Order</h5>
+                        <h5 class="text-base font-bold">Wishlist</h5>
+                        <div class="w-full border-b border-neutral-200 dark:border-neutral-700"></div>
+                    </div>
+                    <div class="UserProfile__item3">
+                        <h5 class="text-base font-bold">Dark theme</h5>
+                        <h5 class="text-base font-bold">Help</h5>
+                        <h5 class="text-base font-bold">Log out</h5>
+                        
+                    </div>
+                </div>
+
             </div>
+
         </div>
 
-
+    </div>
 </template>
 
 
 <style scoped>
+.UserProfile__items {
+    display: flex;
+    flex-direction: column;
+    /* padding: 6px; */
+    gap: 2px;
+    align-items: center;
+    text-align:justify;
+    height: auto;
+    width: auto;
+    padding: 2rem;
+    cursor: pointer;
+    margin: 0px 3px;
+}
+.UserProfile__items .UserProfile__item1__img {
+    display: inline-block;
+    flex-direction: row;
+    justify-content: flex-start;
+    border-radius:100%;
+
+
+}
 </style>
